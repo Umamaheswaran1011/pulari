678bfec9f302891a2114717dae87872d24df3a87
Umamaheswaran1011 <mpuma1011@gmail.com>
2026-02-20 16:43:10 +0530

    final commit

--- pulari_final/products/models.py
+++ pulari_final/products/models.py
@@ -34,4 +34,5 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        
         return f"Order #{self.id} - {self.customer_name} ({self.product})"
